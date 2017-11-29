e4c1e5568a7bc69a0b3c3a94ce9da191d8b1960d
haohong725 <haohong725@qq.com>
2017-11-29 22:37:34 +0800

    x

--- src/main/java/com/haohong/ch01/BinarySearch.java
+++ src/main/java/com/haohong/ch01/BinarySearch.java
@@ -1,16 +1,16 @@
 package com.haohong.ch01;
 
 public class BinarySearch {
-    public static int rank(int key, int[] a) {
+    public static int rank(int value, int[] a) {
         int low = 0;
         int high = a.length - 1;
 
         while (low <= high) {
             int middle = low + (high - low) / 2;
 
-            if (key < a[middle]) {
+            if (value < a[middle]) {
                 high = middle - 1;
-            } else if (key > a[middle]) {
+            } else if (value > a[middle]) {
                 low = middle + 1;
             } else {
                 return middle;
