2fc7e42026ab79eb34a17be52b20282bc97026a1
haohong725 <haohong725@qq.com>
2017-12-05 22:29:44 +0800

    test 4k ints sort by Selection

--- src/test/java/com/haohong/ch02/SelectionTest.java
+++ src/test/java/com/haohong/ch02/SelectionTest.java
@@ -1,15 +1,29 @@
 package com.haohong.ch02;
 
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
 import junit.framework.TestCase;
 
-/**
- * Created by haohong on 04/12/2017.
- */
+import java.util.Arrays;
+
 public class SelectionTest extends TestCase {
     public void test() {
         Integer[] ints = {1, -1, 3, 9};
 
         Selection.sort(ints);
         assert Selection.isSorted(ints);
+    }
+
+    public void test4Kints() {
+        int[] ints = new In("src/test/resource/4Kints.txt").readAllInts();
+
+        Integer[] integers = Arrays.stream(ints).boxed().toArray(Integer[]::new);
+
+//        Selection.show(integers);
+
+        Selection.sort(integers);
+        assert  Selection.isSorted(integers);
+
+//        Selection.show(integers);
     }
 }
