6bfca53d63e92344b22ce8a5ad1995f59d2f4191
haohong725 <haohong725@qq.com>
2017-12-06 21:56:58 +0800

    X

--- src/main/java/com/haohong/ch02/Selection.java
+++ src/main/java/com/haohong/ch02/Selection.java
@@ -2,7 +2,7 @@ package com.haohong.ch02;
 
 import edu.princeton.cs.algs4.StdOut;
 
-public class Selection {
+public class Selection extends BaseSort {
     public static void sort(Comparable[] a) {
         int N = a.length;
 
@@ -15,32 +15,5 @@ public class Selection {
             }
             exch(a,i, min);
         }
-    }
-
-    private static boolean less(Comparable v, Comparable w) {
-        return v.compareTo(w) < 0;
-    }
-
-    private static void exch(Comparable[] a, int i, int j) {
-        Comparable t = a[i];
-        a[i] = a[j];
-        a[j] = t;
-    }
-
-    public static void show(Comparable[] a) {
-        for (int i = 0; i < a.length; i++) {
-            StdOut.print(a[i] + " ");
-        }
-        StdOut.println();
-    }
-
-    public static boolean isSorted(Comparable[] a) {
-        for (int i = 1; i < a.length; i++) {
-            if (less(a[i], a[i-1])) {
-                return false;
-            }
-        }
-
-        return true;
     }
 }
