30bec705f321a2f90c51fccbc634831a57215565
haohong725 <haohong725@qq.com>
2017-12-09 21:49:31 +0800

    add Merge Sort

---
+++ src/test/java/com/haohong/ch02/MergeTest.java
@@ -0,0 +1,41 @@
+package com.haohong.ch02;
+
+import edu.princeton.cs.algs4.In;
+import junit.framework.TestCase;
+
+import java.util.Arrays;
+
+public class MergeTest extends TestCase {
+    public void testMerge() {
+        int[] ints = {1, 3, 5, 2, 4, 6, 7};
+
+        Integer[] integers = Arrays.stream(ints).boxed().toArray(Integer[]::new);
+
+        
+        Merge.merge(integers, 0, ints.length/2-1, ints.length-1);
+
+        for (Integer integer:
+             integers) {
+            System.out.println(integer);
+        }
+    }
+
+    public void test() {
+        Integer[] ints = {1, -1, 3, 9};
+
+        Merge.sort(ints);
+        assert Merge.isSorted(ints);
+    }
+
+    public void test4Kints() {
+        int[] ints = new In("src/test/resource/1Mints.txt").readAllInts();
+
+        Integer[] integers = Arrays.stream(ints).boxed().toArray(Integer[]::new);
+
+//        Selection.show(integers);
+
+        Merge.sort(integers);
+        assert  Merge.isSorted(integers);
+    }
+
+}
