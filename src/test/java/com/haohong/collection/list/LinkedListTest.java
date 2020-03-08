3b4358b03855b98b23cb3cc8b920720b4426a119
haohong725 <haohong725@qq.com>
2020-03-08 21:08:37 +0800

    x

--- src/test/java/com/haohong/collection/list/LinkedListTest.java
+++ src/test/java/com/haohong/collection/list/LinkedListTest.java
@@ -1,7 +1,32 @@
 package com.haohong.collection.list;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class LinkedListTest extends ListTest {
     protected List<Integer> generateList() {
         return new LinkedList<Integer>();
+    }
+
+    @Test
+    public void huhuhubhuih() throws Exception {
+        List<Integer> list = this.generateList();
+
+        list.add(1);
+        list.add(3);
+        list.add(5);
+        list.add(9);
+
+        list.remove(Integer.valueOf(5));
+
+        Assert.assertNull(null);
+
+//        list = null;
+
+        System.gc();
+//        while (true) {
+            System.out.println("sleep");
+//            Thread.sleep(1000);
+//        }
     }
 }
